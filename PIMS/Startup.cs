8ae91731e818b24b918877d53bc3596395ae9ac8
Nilshan Silva <nilshandevinda1@gmail.com>
2017-12-22 14:30:03 +0530

    Add project files.

---
+++ PIMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PIMS.Startup))]
+namespace PIMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
